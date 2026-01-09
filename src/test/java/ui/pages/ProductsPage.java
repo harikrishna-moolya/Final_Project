885c18a9e04da3af05ede80f7a65548c30b62969
harikrishna-moolya <hari.krishna@moolya.com>
2026-01-09 16:21:51 +0530

    ProductsPage.java

--- src/test/java/ui/pages/ProductsPage.java
+++ src/test/java/ui/pages/ProductsPage.java
@@ -278,39 +278,16 @@ public class ProductsPage {
     // --------------------------
     // PRODUCT OPERATIONS
     // --------------------------
-   public void addFirstProductToCart() {
+    public void addFirstProductToCart() {
+        hideAds();
 
-    hideAds();
+        WebElement btn = wait.until(ExpectedConditions.elementToBeClickable(addFirstProduct));
 
-    WebElement addBtn = wait.until(
-        ExpectedConditions.presenceOfElementLocated(
-            By.xpath("(//a[contains(@class,'add-to-cart')])[1]")
-        )
-    );
+        js().executeScript("arguments[0].scrollIntoView({block:'center'});", btn);
+        js().executeScript("arguments[0].click();", btn);
 
-    // Scroll into view (CRITICAL)
-    ((JavascriptExecutor) driver).executeScript(
-        "arguments[0].scrollIntoView({block:'center'});", addBtn
-    );
-
-    // Small wait for animation to settle
-    wait.until(ExpectedConditions.elementToBeClickable(addBtn));
-
-    // JS click (MANDATORY for CI)
-    ((JavascriptExecutor) driver).executeScript(
-        "arguments[0].click();", addBtn
-    );
-
-    // Wait until cart count updates OR success message
-    wait.until(ExpectedConditions.or(
-        ExpectedConditions.visibilityOfElementLocated(
-            By.xpath("//a[contains(text(),'View Cart')]")
-        ),
-        ExpectedConditions.visibilityOfElementLocated(
-            By.xpath("//span[@class='cart-count' and text()!='0']")
-        )
-    ));
-}
+        hideAds();
+    }
 
 
     public void clickContinueShopping() {
