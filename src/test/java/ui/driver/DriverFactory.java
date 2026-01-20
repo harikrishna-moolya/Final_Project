b8178185d49bbfbc14dbb5deb521802e5cc919e2
harikrishna-moolya <hari.krishna@moolya.com>
2026-01-20 15:00:06 +0530

    Update DriverFactory.java

--- src/test/java/ui/driver/DriverFactory.java
+++ src/test/java/ui/driver/DriverFactory.java
@@ -1,37 +1,4 @@
-/*
-package ui.driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-
-public class DriverFactory {
-
-    private static WebDriver driver;
-
-    public static WebDriver getDriver() {
-
-        if (driver == null) {
-            WebDriverManager.chromedriver().setup();
-
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--incognito");
-            options.addArguments("--start-maximized");
-
-            driver = new ChromeDriver(options);
-        }
-        return driver;
-    }
-
-    public static void quitDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
-    }
-}
-*/
 package ui.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
