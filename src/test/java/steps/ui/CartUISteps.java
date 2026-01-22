06ff845130a2946d787207773e83024440fce586
harikrishna-moolya <hari.krishna@moolya.com>
2026-01-22 10:44:37 +0530

    Update CartUISteps.java

--- src/test/java/steps/ui/CartUISteps.java
+++ src/test/java/steps/ui/CartUISteps.java
@@ -1,20 +1,4 @@
-/*
-package steps.ui;
 
-import io.cucumber.java.en.When;
-import ui.driver.DriverFactory;
-import ui.pages.CartPage;
-
-public class CartUISteps {
-
-    CartPage cart = new CartPage(DriverFactory.getDriver());
-
-    @When("I click checkout on cart page")
-    public void checkout() {
-        cart.clickCheckout();
-    }
-}
-*/
 package steps.ui;
 
 import io.cucumber.java.en.When;
