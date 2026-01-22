6ce490bfbbe503bb1ae72794f87ae11825681112
harikrishna-moolya <hari.krishna@moolya.com>
2026-01-22 10:47:55 +0530

    Update LoggerClass.java

--- src/test/java/utils/LoggerClass.java
+++ src/test/java/utils/LoggerClass.java
@@ -1,29 +1,4 @@
-/*
-package logger;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class LoggerClass {
-
-    public static Logger getLogger(Class<?> clazz) {
-        return LogManager.getLogger(clazz);
-    }
-}
-*//*
-
-package logger;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class LoggerClass {
-
-    public static Logger getLogger(Class<?> cls) {
-        return LogManager.getLogger(cls);
-    }
-}
-*/
 package utils;
 
 import org.apache.logging.log4j.LogManager;
