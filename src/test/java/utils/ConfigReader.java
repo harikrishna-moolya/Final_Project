4132b7e58e478dfa9031eb5783b18778a06f979b
harikrishna-moolya <hari.krishna@moolya.com>
2026-01-22 10:47:35 +0530

    Update ConfigReader.java

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -1,27 +1,4 @@
-/*
-package base;
 
-import java.io.FileInputStream;
-import java.util.Properties;
-
-public class ConfigReader {
-
-    static Properties prop = new Properties();
-
-    static {
-        try {
-            FileInputStream fis = new FileInputStream("src/main/resources/config.properties");
-            prop.load(fis);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String get(String key) {
-        return prop.getProperty(key);
-    }
-}
-*/
 package utils;
 
 import java.io.FileInputStream;
